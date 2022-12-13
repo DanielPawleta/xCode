59071d1e97e3a23692a62bf94168a11c1d719901
DanielPawleta <tyla@op.pl>
2022-12-13 22:33:28 +0100

    First commit

---
+++ src/test/java/com/example/xCode/XCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
